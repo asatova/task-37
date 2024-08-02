5512ed408196230b7dd3c6ec46f9202ff2f22584
zarnigor <zarnigorasatova52@gmail.com>
2024-08-02 11:13:22 +0500

    feature

--- task-37/src/App.js
+++ task-37/src/App.js
@@ -1,29 +1,86 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
 class App extends Component {
     state = {
-        firsName: "",
+        firstName: "",
+        email: "",
+        message: "",
+        nativeLanguage: "",
     };
-    handelValue = (e) => {
+
+    handleValue = (e) => {
         this.setState({
-            firstName:e.target.value,
-        })
-    }
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    validateInput = (e) => {
+        if (e.target.value.length < 4) {
+            alert("4 tadan kop harf kiriting");
+        }
+    };
+
     render() {
-        return <div className="container">
-            <form>
-                <label htmlFor="firstName">First name</label>
-                <input
-                className="form-control"
-                type="text"
-                name="firstName"
-                id="firstName"
-                value={this.state.firstName}
-                onChange={this.handelValue}
-                />
-            </form>
-        </div>
+        const { firstName, email, message, nativeLanguage } = this.state;
+        return (
+            <div className="container">
+                <form>
+                    <div>
+                        <label htmlFor="firstName">First name</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="firstName"
+                            id="firstName"
+                            value={firstName}
+                            onChange={this.handleValue}
+                            onBlur={this.validateInput}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            className="form-control"
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            onChange={this.handleValue}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message</label>
+                        <textarea
+                            className="form-control"
+                            name="message"
+                            id="message"
+                            cols="30"
+                            rows="10"
+                            value={message}
+                            onChange={this.handleValue}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="nativeLanguage">Native Language</label>
+                        <select
+                            className="form-control"
+                            name="nativeLanguage"
+                            id="nativeLanguage"
+                            value={nativeLanguage}
+                            onChange={this.handleValue}
+                        >
+                            <option value="">Select a language</option>
+                            <option value="uz">Uzbek</option>
+                            <option value="ru">Russian</option>
+                            <option value="eng">English</option>
+                        </select>
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
-export  default App
+export default App;
